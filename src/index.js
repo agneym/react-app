b0c0ce4cee520b6265ebeb9a178bc2d7cf70a7fa
BoyWithSilverWings <agney.menons@live.com>
2017-10-01 18:55:10 +0530

    Setting up prettier to match jsx files

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import 'core-js/es6/map';
-import 'core-js/es6/set';
+//Adding support for <IE11 because React 16 uses Map and Set. These are not natively supported on these browsers.
+import "core-js/es6/map";
+import "core-js/es6/set";
 
 if (module.hot) {
   module.hot.accept();
@@ -18,16 +19,22 @@ if (module.hot) {
   });
 }
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/sw.js').then(function (registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function (err) {
-      // Registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker.register("/sw.js").then(
+      function(registration) {
+        // Registration was successful
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function(err) {
+        // Registration failed :(
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
   });
 }
 
-ReactDOM.render(<App />, document.getElementById("root")); 
+ReactDOM.render(<App />, document.getElementById("root"));
